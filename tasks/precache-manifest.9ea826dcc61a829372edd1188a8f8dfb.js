1ea462c1853a72820386fa78e087941b5d3180d6
Sebastian Springer <info@sebastian-springer.com>
2019-06-04 22:55:47 +0200

    added stuff

---
+++ tasks/precache-manifest.9ea826dcc61a829372edd1188a8f8dfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab3074d180ab5305348bc370eb5ea044",
+    "url": "/tasks/index.html"
+  },
+  {
+    "revision": "6e3b204d8e97e596babb",
+    "url": "/tasks/static/css/main.92010188.chunk.css"
+  },
+  {
+    "revision": "3299ba32cf7c372c1efa",
+    "url": "/tasks/static/js/2.8b90c81f.chunk.js"
+  },
+  {
+    "revision": "6e3b204d8e97e596babb",
+    "url": "/tasks/static/js/main.a5a9a078.chunk.js"
+  },
+  {
+    "revision": "7d8089a231448516824e",
+    "url": "/tasks/static/js/runtime~main.b1405a7c.js"
+  }
+]);
